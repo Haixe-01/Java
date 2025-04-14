44ce0fc7ce4b4c6affdf3fede73af47bfb3e3928
Haixe-01 <princesohi041@gmail.com>
2025-04-14 14:00:53 +0530

    added compount interest calculator code

---
+++ _03_Compund_interest_calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class _03_Compund_interest_calculator {
+    public static void main(String[] args){
+
+        //The formula of compound interest is: amount = Principal * Math.pow(1+(Interest/n),n*time)
+
+        Scanner scanner = new Scanner(System.in);
+
+        int Principal;
+        double Interest;
+        int n;
+        int time;
+        double amount;
+
+        System.out.println("Enter the Principal amount: ");
+        Principal = scanner.nextInt();
+
+        System.out.println("Enter the Interest in %: ");
+        Interest = scanner.nextDouble()/100; //convert to decimal
+
+        System.out.println("Enter the number of times the interest is compounded per year: ");
+        n = scanner.nextInt();
+
+        System.out.println("Enter the total time in years: ");
+        time = scanner.nextInt();
+
+        amount = Principal * Math.pow(1+(Interest/n),n*time);
+
+        System.out.printf("The total amount will be: %.2f",amount);
+
+        scanner.close();
+    }
+}
